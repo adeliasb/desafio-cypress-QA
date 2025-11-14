80a133a1988616a344cd338910fe1e366c69ee66
adeliasb <qa.asbs.lab@gmail.com>
2025-11-14 13:41:24 -0300

    Atualização das informações no arquivo cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,30 @@
+// cypress.config.js
+// Configuração principal do Cypress com integração do preprocessor cucumber
 const { defineConfig } = require("cypress");
+const cucumber = require("cypress-cucumber-preprocessor").default;
 
 module.exports = defineConfig({
   e2e: {
+    // Base da aplicação que vamos testar
+    baseUrl: "http://lojaebac.ebaconline.art.br",
+    // Pattern para encontrar os arquivos feature do cucumber
+    specPattern: "cypress/e2e/features/*.feature",
+    // Arquivo de suporte global
+    supportFile: "cypress/support/e2e.js",
+
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Registra o preprocessor para arquivos feature
+      on("file:preprocessor", cucumber());
+      return config;
     },
+  },
+
+  // Reporter mochawesome para relatórios HTML e JSON
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    overwrite: false,
+    html: true,
+    json: true,
   },
 });
